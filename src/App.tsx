b44ddf885523539e05adb953d3a3dafb807853f4
imelnath <fiomelnathany@gmail.com>
2023-06-07 17:02:38 +0700

    add route

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,24 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from './routes/Home';
-import Store from './routes/Store';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./routes/Homes";
+import Store from "./routes/Store";
+import Books from "./routes/Book";
+import AddBook from "./AddBook";
+import EditBook from "./routes/EditBook";
+import { Navbar } from "./components/Navbar";
 
 export default function App() {
-	return (
-		<Router>
-			<Routes>
-				<Route
-					path='/'
-					element={<Home />}
-				/>
-				<Route
-					path='/:storeId'
-					Component={Store}
-				/>
-			</Routes>
-		</Router>
-	);
+  return (
+    <div>
+      <Navbar />
+      <Router>
+        <Routes>
+          <Route path="/" Component={Home} />
+          <Route path="/:store_number" Component={Store} />
+          <Route path="/:store_number/:book_number" Component={Books} />
+          <Route path="/:store_number/addbooks" Component={AddBook} />
+          <Route path="/:booknumber/editbooks" Component={EditBook} />
+        </Routes>
+      </Router>
+    </div>
+  );
 }
